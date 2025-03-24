03f7f50d1e8fd4ef8dec74cd7c9cdec4b448d9cc
kygm <rickev20@gmail.com>
2025-03-24 16:38:55 -0500

    Restructured some filepaths and the project. Search appears to be working

--- zipCode-Project2/main.cpp
+++ zipCode-Project2/main.cpp
@@ -4,38 +4,44 @@
 #include <unordered_map>
 #include <vector>
 
+using namespace std;
+
 // Function to convert CSV to length-indicated format
-void convertToLengthIndicated(const std::string& inputFile, const std::string& outputFile) {
-    std::ifstream inFile(inputFile);
-    std::ofstream outFile(outputFile);
+void convertToLengthIndicated(const string &inputFile, const string &outputFile)
+{
+    ifstream inFile(inputFile);
+    ofstream outFile(outputFile);
     Buffer buffer;
 
     // Write header
-    std::string header;
-    std::getline(inFile, header);
+    string header;
+    getline(inFile, header);
     buffer.pack(header);
     buffer.writeHeader(outFile);
 
     // Process records
-    std::string line;
-    while (std::getline(inFile, line)) {
+    string line;
+    while (getline(inFile, line))
+    {
         buffer.pack(line);
-        outFile << buffer.unpack() << std::endl;
+        outFile << buffer.unpack() << endl;
     }
 }
 
 // Function to create a primary key index
-std::unordered_map<std::string, size_t> createIndex(const std::string& filename) {
-    std::unordered_map<std::string, size_t> index;
-    std::ifstream file(filename);
+unordered_map<string, size_t> createIndex(const string &filename)
+{
+    unordered_map<string, size_t> index;
+    ifstream file(filename);
     size_t offset = 0;
-    std::string line;
+    string line;
 
     // Skip header
-    std::getline(file, line);
+    getline(file, line);
 
-    while (std::getline(file, line)) {
-        std::string zipCode = line.substr(0, line.find(','));  // Extract Zip Code
+    while (getline(file, line))
+    {
+        string zipCode = line.substr(0, line.find(',')); // Extract Zip Code
         index[zipCode] = offset;
         offset = file.tellg();
     }
@@ -44,44 +50,54 @@ std::unordered_map<std::string, size_t> createIndex(const std::string& filename)
 }
 
 // Function to write index to a file
-void writeIndexToFile(const std::unordered_map<std::string, size_t>& index, const std::string& filename) {
-    std::ofstream outFile(filename);
-    for (const auto& entry : index) {
-        outFile << entry.first << "," << entry.second << std::endl;
+void writeIndexToFile(const unordered_map<string, size_t> &index, const string &filename)
+{
+    ofstream outFile(filename);
+    for (const auto &entry : index)
+    {
+        outFile << entry.first << "," << entry.second << endl;
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     // Convert CSV files to length-indicated format
-    convertToLengthIndicated("original.csv", "original_length_indicated.txt");
-    convertToLengthIndicated("randomized.csv", "randomized_length_indicated.txt");
+    convertToLengthIndicated("resources/zipCodes.csv", "output/original_length_indicated.txt");
+    convertToLengthIndicated("resources/randomized.csv", "output/randomized_length_indicated.txt");
 
     // Create and save primary key indexes
-    auto originalIndex = createIndex("original_length_indicated.txt");
-    writeIndexToFile(originalIndex, "original_index.txt");
+    auto originalIndex = createIndex("output/original_length_indicated.txt");
+    writeIndexToFile(originalIndex, "output/original_index.txt");
 
-    auto randomizedIndex = createIndex("randomized_length_indicated.txt");
-    writeIndexToFile(randomizedIndex, "randomized_index.txt");
+    auto randomizedIndex = createIndex("output/randomized_length_indicated.txt");
+    writeIndexToFile(randomizedIndex, "output/randomized_index.txt");
 
     // Handle command-line arguments
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " -Z<zipCode1> -Z<zipCode2> ..." << std::endl;
+    if (argc < 2)
+    {
+        cerr << "Usage: " << argv[0] << " -Z<zipCode1> -Z<zipCode2> ..." << endl;
         return 1;
     }
 
-    std::vector<std::string> zipCodes;
-    for (int i = 1; i < argc; ++i) {
-        if (std::string(argv[i]).substr(0, 2) == "-Z") {
-            zipCodes.push_back(std::string(argv[i]).substr(2));  // Fix: Convert char* to std::string
+    vector<string> zipCodes;
+    for (int i = 1; i < argc; ++i)
+    {
+        if (string(argv[i]).substr(0, 2) == "-Z")
+        {
+            zipCodes.push_back(string(argv[i]).substr(2)); // Fix: Convert char* to string
         }
     }
 
     // Search for Zip Codes
-    for (const auto& zipCode : zipCodes) {
-        if (originalIndex.find(zipCode) != originalIndex.end()) {
-            std::cout << "Found: " << zipCode << std::endl;
-        } else {
-            std::cout << "Not Found: " << zipCode << std::endl;
+    for (const auto &zipCode : zipCodes)
+    {
+        if (originalIndex.find(zipCode) != originalIndex.end())
+        {
+            cout << "Found: " << zipCode << endl;
+        }
+        else
+        {
+            cout << "Not Found: " << zipCode << endl;
         }
     }
 
